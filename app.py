134c13c06199478fb2cb5eaa6c2fd5c45b200cb9
dstencil <dstencil@cybertrol.com>
2022-12-16 16:19:42 -0600

    changed app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, jsonify
-app = Flask(__name__)@app.route("/")
-def hello():
-    return jsonify({"Message": "Hello World'"})if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+    def hello():
+        return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
